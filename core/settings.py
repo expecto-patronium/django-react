f85e9f34a27b31dc944fe4cb87deeb5044ea3db5
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-04-10 14:28:27 +0500

    django updates

--- core/settings.py
+++ core/settings.py
@@ -76,17 +76,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hasnain_db',
-        'USER': "hasnain",
-        'PASSWORD': '#Galaxyj5',
-        'HOST': '/cloudsql/cloud-work-314310:us-central1:training-week',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
