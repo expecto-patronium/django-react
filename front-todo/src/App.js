dfab805c6fe3a0c60c0247370de094460a736227
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-04-12 15:38:49 +0500

    first commit

---
+++ front-todo/src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginReg from "./pages/auth/LoginReg";
+import Layout from "./pages/Layout";
+import { useSelector } from "react-redux";
+import Todo from "./components/tod";
+import Card from "./components/Card";
+// import "./App.css"
+function App() {
+  const { access_token } = useSelector(state => state.auth)
+
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+          {/* <Route path="/new" element={<Card2 />} /> */}
+
+            {/* <Route path="/" index element={<LoginReg />} /> */}
+            <Route path="login" element={!access_token ? <LoginReg /> : <Navigate to="/home" />} />
+            </Route>
+
+          {/* <Route path="/home" element={access_token ? <Todo /> : <Navigate to="/login" />} /> */}
+          <Route path="/home" element={<Card />} />
+
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
