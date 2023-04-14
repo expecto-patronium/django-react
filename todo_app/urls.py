ad2a8bec9123fa4e61e2b4a6f187cb2532c9f983
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-04-15 01:49:28 +0500

    updated code

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('login/', TemplateView.as_view(template_name='index.html')),
     path('home/', TemplateView.as_view(template_name='index.html')),
-    path("google_user/", views.get_google_user)
+    path("google_user/", views.get_google_user),
+    path("hello/", views.hello),
+
 
 ]
