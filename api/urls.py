c6f7253b14f80eef4c28af9a485166cf440ad1c9
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-04-15 22:58:22 +0500

    updated code

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from api import views
 
 urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
+    path('test/', views.test_api, name='test'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -9,6 +10,11 @@ class HelloView(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+
+@api_view(["GET"])
+def test_api(requests):
+    return Response({"status": "working ok"})
 
 
 class DataView(APIView):
