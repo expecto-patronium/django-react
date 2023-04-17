7741965440bd8ff1a1bfbb1fb331415f4052a42f
Hasnain Khan <muhammad.hasnain@mlsense.ai>
2023-04-17 18:31:18 +0500

    updated code

--- todo/settings.py
+++ todo/settings.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.environ.get('DJANGO_SECRET_HASNAIN')
-SECRET_KEY = "django-insecure-(l8#v&3+puh=%xfpfrwq!=+5xo+%#8f1u7j*)dj2t0@ibm)a(r"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_HASNAIN')
 
 DEBUG = True
 
@@ -41,8 +41,8 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-'http://localhost:3000',
-'http://127.0.0.1:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
 ]
 ROOT_URLCONF = 'todo.urls'
 
@@ -62,23 +62,31 @@ TEMPLATES = [
         },
     },
 ]
+if os.environ.get("ENV") == 'prod':
+    database = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME_HASNAIN'),
+        'USER': os.environ.get('DB_USER_HASNAIN'),
+        'PASSWORD': os.environ.get('DB_PWD_HASNAIN'),
+        'HOST': '/cloudsql/cloud-work-314310:us-central1:training-week'}
+if os.environ.get("ENV") == 'dev':
+    database = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME_HASNAIN'),
+        'USER': os.environ.get('DB_USER_HASNAIN'),
+        'PASSWORD': os.environ.get('DB_PWD_HASNAIN'),
+        'HOST': os.environ.get("DB_HOST_HASNAIN"),
+        "PORT": "3308"}
+else:
+    database = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ.get('DB_NAME_HASNAIN'),
-    #     'USER': os.environ.get('DB_USER_HASNAIN'),
-    #     'PASSWORD': os.environ.get('DB_PWD_HASNAIN'),
-    #     'HOST': os.environ.get("DB_HOST_HASNAIN"),
-    #     "PORT": os.environ.get("DB_PORT_HASNAIN")
-    # },
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': database
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -119,11 +127,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# STATICFILES_DIRS = [
-#     # os.path.join(BASE_DIR, 'front-todo/build/static'),
-#     os.path.join(BASE_DIR, 'front-todo/build'),
-#
-# ]
 STATICFILES_DIRS = (
     os.path.join(os.path.join(BASE_DIR, 'front-todo'), 'build', 'static'),
     os.path.join(os.path.join(BASE_DIR, 'front-todo'), 'build'),
